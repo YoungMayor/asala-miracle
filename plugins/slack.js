52c01dc34afb160f0861dd993bcc5a83dbf7f809
YoungMayor <youngmayor.dev@gmail.com>
2022-10-31 00:46:32 +0100

    Slack integration

--- plugins/slack.js
+++ plugins/slack.js
@@ -1,30 +1,83 @@
+const axios = require('axios');
+import { faker } from "@faker-js/faker";
 class Slack {
-    constructor(axios) {
-        // Create a custom axios instance
-        const api = axios.create({
-            headers: {
-                common: {
-                    'Accept': 'text/plain, application/json, */*',
-                    'Access-Control-Allow-Headers': '*',
-                    // 'Access-Control-Allow-Methods': '*',
-                    'Access-Control-Allow-Origin': '*'
+    constructor() {
+        const slackWebhookURL = process.env.NUXT_ENV_SLACK_WEBHOOK_URL;
+
+        if (!slackWebhookURL) {
+            throw Error("Slack Webhook not configured");
+        }
+
+        let slackAxios = axios.create({
+            baseURL: slackWebhookURL
+        });
+
+        this.slackAxios = slackAxios;
+    }
+
+    async sendNotification(payload) {
+        try {
+            return await this.slackAxios.post("", payload, {
+                headers: {
+                    'Content-Type': 'text/plain;charset=UTF-8',
                 }
-            }
-        })
-
-
-        this.axios = api
-        this.url =
-            "https://hooks.slack.com/services/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";
+            })
+        } catch (error) {
+            const status = error.response.status;
+            console.error(`There was an error, HTTP status code: ${status}`);
+        }
     }
 
     notify(text) {
-        return this.axios.post(this.url, {
-            text
+        return this.sendNotification({
+            attachments: [
+                { text, color: 'green' }
+            ]
+        })
+    }
+
+    sendContactFormMessage(details) {
+        return this.sendNotification({
+            username: "MimX Message Alert",
+            icon_emoji: ":incoming_envelope:",
+            attachments: [{
+                    fallback: "",
+                    color: "#00D000",
+                    fields: [{
+                        "title": "Sender",
+                        "value": `${details.name} <mailto:${details.email}|${details.email}>`,
+                    }, ]
+                },
+                {
+                    fallback: "",
+                    color: "#ffffff",
+                    fields: [{
+                        "title": "Title",
+                        "value": details.title,
+                    }, ]
+                },
+                {
+                    fallback: "",
+                    color: "#00D000",
+                    fields: [{
+                        "title": "Content",
+                        "value": details.text,
+                    }]
+                },
+            ]
+        })
+    }
+
+    demoContactForm() {
+        return this.sendContactFormMessage({
+            email: faker.internet.email(),
+            name: faker.name.fullName(),
+            title: faker.lorem.paragraph(1),
+            text: faker.lorem.paragraph()
         })
     }
 }
 
-export default ({ $axios }, inject) => {
-    inject('slack', new Slack($axios))
+export default ({}, inject) => {
+    inject('slack', new Slack())
 }
